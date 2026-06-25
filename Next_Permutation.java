84e341931c0debbe43865c4deb7a3a65aac44a11
akshaya180 <akshayakannan49@gmail.com>
2026-06-25 11:04:20 +0530

    Add files via upload

---
+++ Next_Permutation.java
@@ -0,0 +1,56 @@
+import  java.util.*;
+
+
+public  class Next_Permutation {
+
+
+    public static void main(String[] args)
+    {
+      int nums[] = {1,2,3};
+
+          nextPermutation(nums);
+            for(int i = 0; i < nums.length; i++)
+             {
+        System.out.print(nums[i] + " ");
+             }
+    }
+         static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while(i>=0 && nums[i] >= nums[i+1])
+        {
+            i--;
+        }
+
+        if(i>=0)
+        {
+            int j = nums.length-1;
+
+            while(nums[j] <= nums[i])
+            {
+                j--;
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+
+       int  left = i + 1;
+       int right = nums.length - 1;
+
+
+       while(left < right )
+       {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+
+
+        left ++ ;
+        right --;
+       }
+        
+    }
+}
